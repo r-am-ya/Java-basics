4601dbaed3bb9e3e2128414e8e775b35872c66fa
r-am-ya <reddykramya1@gmail.com>
2025-09-23 22:01:43 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class add{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a: ");
+        int a = sc.nextInt();
+        System.out.println("enter b: ");
+        int b = sc.nextInt();
+
+        int c = a+b;
+        System.out.println("sum = "+c);
+    }
+}
